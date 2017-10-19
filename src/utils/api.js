c1092e524702d01e26ad82f57f18983ca03d0eab
allen4dev <alanaliagadev@gmail.com>
2017-10-18 23:11:45 -0400

    [Add] soundcloud user endpoints

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
+import config from './../config';
+
 const jplaceholderBaseURL = 'https://jsonplaceholder.typicode.com';
+const soundcloudBaseURL = 'https://api.soundcloud.com';
 
 const api = {
   jplaceholder: {
@@ -18,6 +21,41 @@ const api = {
         const post = await response.json();
 
         return post;
+      },
+    },
+
+    users: {
+      async getUsers() {
+        const url = `${jplaceholderBaseURL}/users`;
+        const response = await fetch(url);
+        const users = await response.json();
+
+        return users;
+      },
+    },
+  },
+
+  soundcloud: {
+    users: {
+      async getSingle(id) {
+        const url = `
+          ${soundcloudBaseURL}/users/${id}?client_id=${config.soundcloud
+          .CLIENT_ID}`;
+
+        const response = await fetch(url);
+        const user = await response.json();
+
+        return user;
+      },
+
+      async searchUsers(term) {
+        const url = `${soundcloudBaseURL}/users?q=${term}&client_id=${config
+          .soundcloud.CLIENT_ID}`;
+
+        const response = await fetch(url);
+        const users = await response.json();
+
+        return users;
       },
     },
   },

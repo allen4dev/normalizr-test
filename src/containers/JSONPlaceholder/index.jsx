09ed77ecd8e29077ffeb20a2a236760427c6d9f7
allen4dev <alanaliagadev@gmail.com>
2017-10-18 23:17:04 -0400

    [Add] base users feature

--- src/containers/JSONPlaceholder/index.jsx
+++ src/containers/JSONPlaceholder/index.jsx
@@ -1,42 +1,15 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
-import PostList from './../../modules/posts/components/PostList';
-
-import posts from './../../modules/posts';
-
-import helpers from './../../utils/helpers';
+// import { connect } from 'react-redux';
 
 class JSONPlaceholder extends Component {
   constructor(props) {
     super(props);
 
-    this.fetchRandomPost = this.fetchRandomPost.bind(this);
-
     this.state = { loading: true };
   }
 
   componentDidMount() {
-    const { items } = this.props;
-    if (items.length === 0) {
-      this.fetchData();
-    }
-
     this.setState({ loading: false });
-  }
-
-  async fetchData() {
-    const { fetchPosts } = this.props;
-
-    this.setState({ loading: true });
-    await fetchPosts();
-  }
-
-  async fetchRandomPost() {
-    const randomID = helpers.fromOneTo(100);
-    const { fetchPost } = this.props;
-
-    await fetchPost(randomID);
   }
 
   render() {
@@ -46,21 +19,10 @@ class JSONPlaceholder extends Component {
 
     return (
       <div className="JPlaceholder container">
-        <h1 className="title">Posts</h1>
-        <button onClick={this.fetchRandomPost}>Fetch random Post</button>
-        <PostList posts={this.props.items} />
+        <h1 className="title">Users</h1>
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    items: Object.values(state.posts.entities),
-  };
-}
-
-export default connect(mapStateToProps, {
-  fetchPosts: posts.actions.fetchPosts,
-  fetchPost: posts.actions.fetchPost,
-})(JSONPlaceholder);
+export default JSONPlaceholder;
